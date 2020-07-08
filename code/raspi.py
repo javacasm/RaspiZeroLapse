dda1c89fd7167e69fb3e0cdc358275004d24e38d
Javacasm <javacasm@gmail.com>
2020-07-09 00:16:24 +0200

    update format

--- code/raspi.py
+++ code/raspi.py
@@ -5,21 +5,35 @@ import utils
 
 v = '0.2'
 
-cmdGetTrottled = ['/opt/vc/bin/vcgencmd', 'get_throttled'] cmdGetTemp = ['/opt/vc/bin/vcgencmd', 'measure_temp']
+cmdGetTrottled = ['/opt/vc/bin/vcgencmd', 'get_throttled'] 
+cmdGetTemp = ['/opt/vc/bin/vcgencmd', 'measure_temp']
 
-def init(): utils.myLog('RaspiUtils ' + v)
+def init(): 
+    utils.myLog('RaspiUtils ' + v)
 
-def executeCommand(command): stream = os.popen(command) output = stream.read() return output
+def executeCommand(command): 
+    stream = os.popen(command) 
+    output = stream.read() 
+    return output
 
 
-def executeProcess(command, arguments): process = subprocess.Popen([command, arguments], stdout=subprocess.PIPE, 
-                     stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate() return stdout, stderr
+def executeProcess(command, arguments): 
+    process = subprocess.Popen([command, arguments], 
+                    stdout=subprocess.PIPE, 
+                    stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate() 
+    return stdout, stderr
 
 def getTemp():
     # /opt/vc/bin/vcgencmd get_throttled
-    strTemp = executeCommand(cmdGetTemp[0] + ' ' + cmdGetTemp[1]) utils.myLog(strTemp) return strTemp
+    strTemp = executeCommand(cmdGetTemp[0] + ' ' + cmdGetTemp[1]) 
+    utils.myLog(strTemp) 
+    return strTemp
 
-def getThrottled(): strThrotled, strError = executeProcess(cmdGetTrottled[0],cmdGetTrottled[1]) strThrotled = 
-    str(strThrotled) utils.myLog(strThrotled) if strError != None:
-        utils.myLog(strError) return str(strThrotled)
+def getThrottled(): 
+    strThrotled, strError = executeProcess(cmdGetTrottled[0],cmdGetTrottled[1]) 
+    strThrotled = str(strThrotled) 
+    utils.myLog(strThrotled) 
+    if strError != None:
+        utils.myLog(strError) 
+    return str(strThrotled)

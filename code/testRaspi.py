c9a6f1d268fa38a7003873d1696a9d05aff12dea
Javacasm <javacasm@gmail.com>
2020-07-09 00:07:57 +0200

    update

--- code/testRaspi.py
+++ code/testRaspi.py
@@ -1,9 +1,13 @@
 import raspi
 
-v = '0.1'
+v = '0.3'
 
 raspi.init()
 
 temp = raspi.getTemp()
 
 print('Temperatura: ' + temp)
+
+throttled = raspi.getThrottled()
+
+print('Throtted: ' + throttled)

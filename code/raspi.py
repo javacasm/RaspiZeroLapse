0d1d6b15d8f59d43cf00bb61016a6636ab14af41
Javacasm <javacasm@gmail.com>
2020-07-09 21:26:56 +0200

    update

--- code/raspi.py
+++ code/raspi.py
@@ -1,9 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""" Raspberry stuff
+    Licencia CC by @javacasm    
+    Julio de 2020
+"""
 import os
 import subprocess
 
 import utils
 
 v = '0.4'
+
+# https://www.raspberrypi.org/documentation/raspbian/applications/vcgencmd.md
 
 cmdGetTrottled = ['/opt/vc/bin/vcgencmd', 'get_throttled'] 
 cmdGetTemp = ['/opt/vc/bin/vcgencmd', 'measure_temp']
